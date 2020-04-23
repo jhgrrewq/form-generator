5c410e9ab90a835af1594bbd2b59307cbd8190f7
jhgrrewq <zj@citycloud.com.cn>
2020-04-23 14:49:26 +0800

    feat(demo): word 接入默认测试数据

--- demo/src/views/word/src/word.vue
+++ demo/src/views/word/src/word.vue
@@ -3,13 +3,14 @@
 </template>
 <script>
   import WordTemplate from 'packages/cp-word-form'
+  import htmlStr from './html.js'
   export default {
     components: {
       WordTemplate
     },
     data() {
       return {
-        text: 'adfadfa'
+        text: htmlStr
       }
     }
   }
